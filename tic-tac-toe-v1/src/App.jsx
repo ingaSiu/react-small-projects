8419bde3f4c92fad1f2bea08007b3f9cb993cf54
ingaSiu <ingasiud@gmail.com>
2023-10-04 15:28:10 +0300

    add squares on board

--- tic-tac-toe-v1/src/App.jsx
+++ tic-tac-toe-v1/src/App.jsx
@@ -1,5 +1,79 @@
+import './App.css';
+
+import Square from './Components/Square';
+import { useState } from 'react';
+
 const App = () => {
-  return <h1>Hello</h1>;
+  const [board, setBoard] = useState(['', '', '', '', '', '', '', '', '']);
+
+  return (
+    <div className="App">
+      <div className="board">
+        <div className="row">
+          <Square
+            value={board[0]}
+            chooseSquare={() => {
+              alert(0);
+            }}
+          />
+          <Square
+            value={board[1]}
+            chooseSquare={() => {
+              alert(1);
+            }}
+          />
+          <Square
+            value={board[2]}
+            chooseSquare={() => {
+              alert(2);
+            }}
+          />
+        </div>
+
+        <div className="row">
+          <Square
+            value={board[3]}
+            chooseSquare={() => {
+              alert(3);
+            }}
+          />
+          <Square
+            value={board[4]}
+            chooseSquare={() => {
+              alert(4);
+            }}
+          />
+          <Square
+            value={board[5]}
+            chooseSquare={() => {
+              alert(5);
+            }}
+          />
+        </div>
+
+        <div className="row">
+          <Square
+            value={board[6]}
+            chooseSquare={() => {
+              alert(6);
+            }}
+          />
+          <Square
+            value={board[7]}
+            chooseSquare={() => {
+              alert(7);
+            }}
+          />
+          <Square
+            value={board[8]}
+            chooseSquare={() => {
+              alert(8);
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
