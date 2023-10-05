bff724513c86c887ddec1e51847fc32f4f012992
ingaSiu <ingasiud@gmail.com>
2023-10-05 12:08:03 +0300

    add board

--- tic-tac-toe-v2/src/App.jsx
+++ tic-tac-toe-v2/src/App.jsx
@@ -1,7 +1,21 @@
-import './App.css';
+import Cell from './components/Cell';
+import { useState } from 'react';
 
 const App = () => {
-  return <div>Hello</div>;
+  const [cells, setCells] = useState(['', '', '', '', '', '', '', '', '']);
+  const [go, setGo] = useState('circle');
+  const [winningMessage, setWinningMessage] = useState('');
+  return (
+    <div className="app">
+      <div className="gameBoard">
+        {cells.map((cell, index) => (
+          <Cell key={index} id={index} cell={cell} />
+        ))}
+
+        <p></p>
+      </div>
+    </div>
+  );
 };
 
 export default App;
