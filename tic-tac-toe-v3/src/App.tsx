d2086b9aec9dea4e5705f8442f6350a5c6384103
ingaSiu <ingasiud@gmail.com>
2023-10-06 14:00:14 +0300

    import board component

--- tic-tac-toe-v3/src/App.tsx
+++ tic-tac-toe-v3/src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css';
 
+import Board from './components/Board/Board';
+
 const App = () => {
   return (
     <div className="game">
       <h1>Tic-Tac-Toe game</h1>
+      <Board board={[]} handleClick={() => ''} />
     </div>
   );
 };
