1ccafee345670d1adaf2949e81cf1c84df5b361e
ingaSiu <ingasiud@gmail.com>
2023-10-06 14:00:03 +0300

    add board component

---
+++ tic-tac-toe-v3/src/components/Board/Board.tsx
@@ -0,0 +1,22 @@
+type BoardProps = {
+  board: Array<Array<string | null>>;
+  handleClick: (row: number, col: number) => void;
+};
+
+const Board = ({ board, handleClick }: BoardProps) => {
+  return (
+    <div className="board">
+      {board.map((row, rowIndex) => (
+        <div key={rowIndex} className="boardRow">
+          {row.map((cell, cellIndex) => (
+            <button key={cellIndex} className="cell" onClick={() => handleClick(rowIndex, cellIndex)}>
+              {cell}
+            </button>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Board;
