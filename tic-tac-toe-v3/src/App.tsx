c55a6e25bad7542c804590ae95427c27b6f48588
ingaSiu <ingasiud@gmail.com>
2023-10-07 10:10:59 +0300

    add board

--- tic-tac-toe-v3/src/App.tsx
+++ tic-tac-toe-v3/src/App.tsx
@@ -1,12 +1,14 @@
-import './App.css';
+import './App.scss';
 
 import Board from './components/Board/Board';
+import { useState } from 'react';
 
 const App = () => {
+  const [board, setBoard] = useState<any>(Array.from({ length: 3 }, () => Array.from({ length: 3 }, () => null)));
   return (
     <div className="game">
       <h1>Tic-Tac-Toe game</h1>
-      <Board board={[]} handleClick={() => ''} />
+      <Board board={board} handleClick={() => ''} />
     </div>
   );
 };
