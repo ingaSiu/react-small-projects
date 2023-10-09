d24e3a5c9e743a6a01d52711946d2e2ea77a38ba
ingaSiu <ingasiud@gmail.com>
2023-10-09 10:44:44 +0300

    add computer move logic

--- tic-tac-toe-v3/src/App.tsx
+++ tic-tac-toe-v3/src/App.tsx
@@ -5,6 +5,43 @@ import { useState } from 'react';
 
 type BoardArray = Array<Array<string | null>>;
 
+const makeComputerMove = (board: BoardArray): [number, number] => {
+  const emptyCell: [number, number][] = [];
+  board.forEach((row, rowIndex) => {
+    row.forEach((cell, cellIndex) => {
+      if (!cell) {
+        emptyCell.push([rowIndex, cellIndex]);
+      }
+    });
+  });
+
+  const randomIndex = Math.floor(Math.random() * emptyCell.length);
+  return emptyCell[randomIndex];
+};
+
+const checkWinner = (board: BoardArray): string | null => {
+  const lines = [
+    //Rows
+    [board[0][0], board[0][1], board[0][2]],
+    [board[1][0], board[1][1], board[1][2]],
+    [board[2][0], board[2][1], board[2][2]],
+    //columns
+    [board[0][0], board[1][0], board[2][0]],
+    [board[0][1], board[1][1], board[2][1]],
+    [board[0][2], board[1][2], board[2][2]],
+
+    //Diagonals
+    [board[0][0], board[1][1], board[2][2]],
+    [board[0][2], board[1][1], board[2][0]],
+  ];
+  for (const line of lines) {
+    if (line[0] && line[0] === line[1] && line[1] === line[2]) {
+      return line[0];
+    }
+  }
+  return null;
+};
+
 const App = () => {
   const [board, setBoard] = useState<BoardArray>(
     Array.from({ length: 3 }, () => Array.from({ length: 3 }, () => null)),
@@ -15,29 +52,6 @@ const App = () => {
 
   // function returns string
   // type cell = 'x' | 'o';
-
-  const checkWinner = (board: BoardArray): string | null => {
-    const lines = [
-      //Rows
-      [board[0][0], board[0][1], board[0][2]],
-      [board[1][0], board[1][1], board[1][2]],
-      [board[2][0], board[2][1], board[2][2]],
-      //columns
-      [board[0][0], board[1][0], board[2][0]],
-      [board[0][1], board[1][1], board[2][1]],
-      [board[0][2], board[1][2], board[2][2]],
-
-      //Diagonals
-      [board[0][0], board[1][1], board[2][2]],
-      [board[0][2], board[1][1], board[2][0]],
-    ];
-    for (const line of lines) {
-      if (line[0] && line[0] === line[1] && line[1] === line[2]) {
-        return line[0];
-      }
-    }
-    return null;
-  };
 
   const handleOnClick = (row: number, col: number) => {
     if (board[row][col] || winner) {
@@ -53,6 +67,16 @@ const App = () => {
     const newWinner = checkWinner(updatedPlayerBoard);
     setWinner(newWinner);
     setPlayer('X');
+
+    // Computer's move
+    if (!newWinner) {
+      const [computerRow, computerCol] = makeComputerMove(updatedPlayerBoard);
+      const updatedComputerBoard = updatedPlayerBoard.map((newRow, rowIndex) =>
+        newRow.map((cell, cellIndex) => (rowIndex === computerRow && cellIndex === computerCol ? 'O' : cell)),
+      );
+      setBoard(updatedComputerBoard);
+      setWinner(checkWinner(updatedComputerBoard));
+    }
   };
 
   return (
