d59f4ed58cae133a52a1f1bbef1d762d3adef8bc
ingaSiu <ingasiud@gmail.com>
2023-10-10 10:38:12 +0300

    check opponent moves

--- tic-tac-toe-v4/src/utils/getCleverMoves.tsx
+++ tic-tac-toe-v4/src/utils/getCleverMoves.tsx
@@ -14,11 +14,29 @@ const getCleverMoves = (board, player, checkWinner) => {
     }),
   );
 
+  // opponent moves
+
+  const opponent = player === 'X' ? 'O' : 'X';
+
+  board.some((row, rowIndex) =>
+    row.some((col, colIndex) => {
+      if (!board[rowIndex][colIndex]) {
+        const clonedBoard = board.map((r) => [...r]);
+
+        clonedBoard[rowIndex][colIndex] = opponent;
+
+        if (checkWinner(clonedBoard) === opponent) {
+          cleverMoves.push([rowIndex][colIndex]);
+          return true;
+        }
+        return false;
+      }
+    }),
+  );
+
   if (cleverMoves.length > 0) {
     return cleverMoves[0];
   }
-
-  // oponnent moves
 
   // choose the center cell
 
