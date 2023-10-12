47105abe26132c0ddf3a00d4dd041ae3eb8d6919
ingaSiu <ingasiud@gmail.com>
2023-10-12 11:27:24 +0300

    make a board

--- tic-tac-toe-v5/src/App.jsx
+++ tic-tac-toe-v5/src/App.jsx
@@ -1,8 +1,27 @@
 import './App.css';
 
 import Square from './components/Square/Square';
+
 const App = () => {
-  return <Square />;
+  return (
+    <>
+      <div className="boardRow">
+        <Square value={1} />
+        <Square value={2} />
+        <Square value={3} />
+      </div>
+      <div className="boardRow">
+        <Square value={4} />
+        <Square value={5} />
+        <Square value={6} />
+      </div>
+      <div className="boardRow">
+        <Square value={7} />
+        <Square value={8} />
+        <Square value={9} />
+      </div>
+    </>
+  );
 };
 
 export default App;
