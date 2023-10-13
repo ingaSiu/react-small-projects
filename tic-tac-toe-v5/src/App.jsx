80681607c90fa0ff2f58dfd248ddcd58f7a9af94
ingaSiu <ingasiud@gmail.com>
2023-10-13 17:16:38 +0300

    add buttons to show history

--- tic-tac-toe-v5/src/App.jsx
+++ tic-tac-toe-v5/src/App.jsx
@@ -90,13 +90,28 @@ const Game = () => {
     setXIsNext(!xIsNext);
   };
 
+  const jumpTo = (nextMove) => {};
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
   return (
     <div className="game">
       <div className="gameBoard">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="gameInfo">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
