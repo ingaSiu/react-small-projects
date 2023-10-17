6d79cdf9ef3e9c8de8f42eebeacb89572236bf2c
ingaSiu <ingasiud@gmail.com>
2023-10-17 12:40:34 +0300

    change func to get winning squares

--- tic-tac-toe-v5/src/utils/calculateWinner.js
+++ tic-tac-toe-v5/src/utils/calculateWinner.js
@@ -14,7 +14,10 @@ export const calculateWinner = (squares) => {
     const [a, b, c] = lines[i];
 
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {
+        winner: squares[a],
+        winningLine: [a, b, c],
+      };
     }
   }
   return null;
