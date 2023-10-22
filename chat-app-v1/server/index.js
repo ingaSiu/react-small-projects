8361a75a24581c5e290cef6cd74539358027c3b3
ingaSiu <ingasiud@gmail.com>
2023-10-22 14:01:06 +0300

    be listening to fe

--- chat-app-v1/server/index.js
+++ chat-app-v1/server/index.js
@@ -16,6 +16,14 @@ const io = new Server(server, {
   },
 });
 
+io.on('connection', (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on('send_message', (data) => {
+    socket.broadcast.emit('receive_message', data);
+  });
+});
+
 server.listen(3000, () => {
   console.log('SERVER IS RUNNING');
 });
