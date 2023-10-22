73b5db309a60f9011fa8b6eb6a953ea6e9d27b2a
ingaSiu <ingasiud@gmail.com>
2023-10-22 14:01:27 +0300

    write msg functionality

--- chat-app-v1/client/src/App.jsx
+++ chat-app-v1/client/src/App.jsx
@@ -1,10 +1,35 @@
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:3000');
+
 const App = () => {
+  const [message, setMessage] = useState('');
+  const [messageReceived, setMessageReceived] = useState('');
+  const sendMessage = () => {
+    socket.emit('send_message', { message });
+  };
+
+  useEffect(() => {
+    socket.on('receive_message', (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
+
   return (
     <div className="container">
-      <input placeholder="Message..." />
-      <button>Send Message</button>
+      <input
+        placeholder="Message..."
+        onChange={(e) => {
+          setMessage(e.target.value);
+        }}
+      />
+      <button onClick={sendMessage}>Send Message</button>
+      <h1>Message:</h1>
+      {messageReceived}
     </div>
   );
 };
