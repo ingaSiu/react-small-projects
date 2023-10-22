b004125f7247d663bf64d87dfda86a1c5206eed7
ingaSiu <ingasiud@gmail.com>
2023-10-22 14:25:07 +0300

    add room

--- chat-app-v1/client/src/App.jsx
+++ chat-app-v1/client/src/App.jsx
@@ -7,10 +7,18 @@ import io from 'socket.io-client';
 const socket = io.connect('http://localhost:3000');
 
 const App = () => {
+  const [room, setRoom] = useState('');
   const [message, setMessage] = useState('');
   const [messageReceived, setMessageReceived] = useState('');
+
+  const joinRoom = () => {
+    if (room !== '') {
+      socket.emit('join_room', room);
+    }
+  };
+
   const sendMessage = () => {
-    socket.emit('send_message', { message });
+    socket.emit('send_message', { message, room });
   };
 
   useEffect(() => {
@@ -21,6 +29,13 @@ const App = () => {
 
   return (
     <div className="container">
+      <input
+        placeholder="Room number..."
+        onChange={(e) => {
+          setRoom(e.target.value);
+        }}
+      />
+      <button onClick={joinRoom}>Join Room</button>
       <input
         placeholder="Message..."
         onChange={(e) => {
