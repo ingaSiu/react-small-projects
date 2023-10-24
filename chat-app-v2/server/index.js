6feb6f8a203dcf9b441b71a749707be96241e12e
ingaSiu <ingasiud@gmail.com>
2023-10-24 14:43:54 +0300

    listening for events and determining recipients in socket.io

--- chat-app-v2/server/index.js
+++ chat-app-v2/server/index.js
@@ -25,9 +25,31 @@ const io = new Server(expressServer, {
 io.on('connection', (socket) => {
   console.log(`User: ${socket.id} connected`);
 
+  // Upon connection - only to usser
+  // .emit goes directly to user
+
+  socket.emit('message', 'Welocome to chat app!');
+
+  // Upon connection - to all others
+  // .broadcast goes to everyone else except the user
+  socket.broadcast.emit('message', `User: ${socket.id.substring(0, 5)} connected`);
+
+  // Listening for a message event
   socket.on('message', (data) => {
     console.log(data);
 
     io.emit('message', `${socket.id.substring(0, 5)}: ${data}`);
+  });
+
+  // when user disconnects - to all others
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('message', `User: ${socket.id.substring(0, 5)} disconnected`);
+  });
+
+  // Listen for activity
+  // everyone else will get a message that we are typing
+
+  socket.on('activity', (name) => {
+    socket.broadcast.emit('activity, name');
   });
 });
