7b8f12bb6e8884707232f179d8764529a7752e18
ingaSiu <ingasiud@gmail.com>
2023-10-25 10:57:27 +0300

    add additional functionality to chat app: client side

--- chat-app-v2/server/public/app.js
+++ chat-app-v2/server/public/app.js
@@ -1,31 +1,50 @@
 const socket = io('ws://localhost:3500');
 
+const msgInput = document.querySelector('#message');
+const nameInput = document.querySelector('#name');
+const chatRoom = document.querySelector('#room');
+
 const activity = document.querySelector('.activity');
-const msgInput = document.querySelector('input');
+const usersList = document.querySelector('.user-list');
+const roomList = document.querySelector('.room-list');
+const chatDisplay = document.querySelector('.chat-display');
 
 const sendMessage = (event) => {
   event.preventDefault();
 
-  if (msgInput.value) {
-    socket.emit('message', msgInput.value);
+  if (nameInput.value && msgInput.value && chatRoom.value) {
+    socket.emit('message', {
+      name: nameInput,
+      text: msgInput.value,
+    });
     msgInput.value = '';
   }
   msgInput.focus();
 };
 
-document.querySelector('form').addEventListener('submit', sendMessage);
+const enterRoom = (e) => {
+  e.preventDefault();
+  if (nameInput.value && chatRoom.value) {
+    socket.emit('enterRoom', {
+      name: nameInput.value,
+      room: chatRoom.value,
+    });
+  }
+};
+
+document.querySelector('.form-msg').addEventListener('submit', sendMessage);
+document.querySelector('.form-join').addEventListener('submit', enterRoom);
+
+msgInput.addEventListener('keypress', () => {
+  socket.emit('activity', nameInput.value);
+});
 
 // Listen for messages
-
 socket.on('message', (data) => {
   activity.textContent = '';
   const listItem = document.createElement('li');
   listItem.textContent = data;
   document.querySelector('ul').appendChild(listItem);
-});
-
-msgInput.addEventListener('keypress', () => {
-  socket.emit('activity', socket.id.substring(0, 5));
 });
 
 let activityTimer;
