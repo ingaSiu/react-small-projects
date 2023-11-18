cf5afa9ed895e340c4c6bb29f3aba33eac44bf29
ingaSiu <ingasiud@gmail.com>
2023-11-18 16:17:15 +0200

    add number formatting

--- calculator-v1/src/App.jsx
+++ calculator-v1/src/App.jsx
@@ -108,6 +108,17 @@ const evaluate = ({ currentOperand, previousOperand, operation }) => {
   return computation.toString();
 };
 
+const INTEGER_FORMATTER = new Intl.NumberFormat('en-us', {
+  maximumFractionDigits: 0,
+});
+
+const formatOperand = (operand) => {
+  if (operand == null) return;
+  const [integer, decimal] = operand.split('.');
+  if (decimal == null) return INTEGER_FORMATTER.format(integer);
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`;
+};
+
 const App = () => {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {});
 
@@ -115,9 +126,9 @@ const App = () => {
     <div className="calculator-grid">
       <div className="output">
         <div className="previous-operand">
-          {previousOperand} {operation}{' '}
+          {formatOperand(previousOperand)} {operation}{' '}
         </div>
-        <div className="current-operand">{currentOperand}</div>
+        <div className="current-operand">{formatOperand(currentOperand)}</div>
       </div>
       <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR })}>
         AC
