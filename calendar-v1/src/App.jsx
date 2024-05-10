dab2e7ea7d746144a213c6886453b2fe8e725ec9
ingaSiu <ingasiud@gmail.com>
2024-05-10 12:38:11 +0300

    clean up

--- calendar-v1/src/App.jsx
+++ calendar-v1/src/App.jsx
@@ -5,11 +5,10 @@ import CalendarHeader from './components/CalendarHeader';
 import Day from './components/Day';
 import DeleteEventModal from './components/DeleteEventModal';
 import NewEventModal from './components/NewEventModal';
+import { useDate } from './hooks/useDate';
 
 const App = () => {
   const [nav, setNav] = useState(0);
-  const [days, setDays] = useState([]);
-  const [dateDisplay, setDateDisplay] = useState('');
   const [clicked, setClicked] = useState();
   const [events, setEvents] = useState(
     localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [],
@@ -23,57 +22,7 @@ const App = () => {
     localStorage.setItem('events', JSON.stringify(events));
   }, [events]);
 
-  useEffect(() => {
-    const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const date = new Date();
-
-    if (nav !== 0) {
-      date.setMonth(new Date().getMonth() + nav);
-    }
-
-    const day = date.getDate();
-    const month = date.getMonth();
-    const year = date.getFullYear();
-
-    const firstDayOfMonth = new Date(year, month, 1);
-    const daysInMonth = new Date(year, month + 1, 0).getDate();
-
-    const dateString = firstDayOfMonth.toLocaleDateString('en-us', {
-      weekday: 'long',
-      year: 'numeric',
-      month: 'numeric',
-      day: 'numeric',
-    });
-
-    const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
-
-    setDateDisplay(`${date.toLocaleDateString('en-us', { month: 'long' })} ${year}`);
-
-    const daysArr = [];
-
-    for (let i = 1; i <= paddingDays + daysInMonth; i++) {
-      const dayString = `${month + 1}/${i - paddingDays}/${year}`;
-
-      if (i > paddingDays) {
-        daysArr.push({
-          value: i - paddingDays,
-          event: eventForDate(dayString),
-          isCurrentDay: i - paddingDays === day && nav === 0,
-          date: dayString,
-        });
-      } else {
-        daysArr.push({
-          value: 'padding',
-          event: null,
-          isCurrentDay: false,
-          date: '',
-        });
-      }
-    }
-
-    setDays(daysArr);
-  }, [events, nav]);
-
+  const { days, dateDisplay } = useDate(events, nav);
   return (
     <>
       <div id="container">
