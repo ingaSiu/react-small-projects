792119624b6b61a9053959ff212d09b4d42975de
ingaSiu <ingasiud@gmail.com>
2024-05-10 11:35:16 +0300

    add functionality for rendering days

--- calendar-v1/src/App.jsx
+++ calendar-v1/src/App.jsx
@@ -1,4 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+import CalendarHeader from './components/CalendarHeader';
+import Day from './components/Day';
 
 const App = () => {
   const [nav, setNav] = useState(0);
@@ -12,10 +15,89 @@ const App = () => {
   const eventForDate = (date) => {
     events.find((e) => e.date === date);
   };
+
+  useEffect(() => {
+    localStorage.setItem('events', JSON.stringify(events));
+  }, [events]);
+
+  useEffect(() => {
+    const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const date = new Date();
+
+    if (nav !== 0) {
+      date.setMonth(new Date().getMonth() + nav);
+    }
+
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+
+    const firstDayOfMonth = new Date(year, month, 1);
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+    const dateString = firstDayOfMonth.toLocaleDateString('en-us', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+    });
+
+    const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+
+    setDateDisplay(`${date.toLocaleDateString('en-us', { month: 'long' })} ${year}`);
+
+    const daysArr = [];
+
+    for (let i = 1; i <= paddingDays + daysInMonth; i++) {
+      const dayString = `${month + 1}/${i - paddingDays}/${year}`;
+
+      if (i > paddingDays) {
+        daysArr.push({
+          value: i - paddingDays,
+          event: eventForDate(dayString),
+          isCurrentDay: i - paddingDays === day && nav === 0,
+          date: dayString,
+        });
+      } else {
+        daysArr.push({
+          value: 'padding',
+          event: null,
+          isCurrentDay: false,
+          date: '',
+        });
+      }
+    }
+
+    setDays(daysArr);
+  }, [events, nav]);
+
   return (
-    <>
-      <div>hello</div>
-    </>
+    <div id="container">
+      <CalendarHeader />
+      <div id="weekdays">
+        <div>Sunday</div>
+        <div>Monday</div>
+        <div>Tuesday</div>
+        <div>Wednesday</div>
+        <div>Thursday</div>
+        <div>Friday</div>
+        <div>Saturday</div>
+      </div>
+
+      <div id="calendar">
+        {days.map((day, index) => (
+          <Day
+            key={index}
+            day={day}
+            onClick={() => {
+              if (day.value !== 'padding') {
+                setClicked(day.date);
+              }
+            }}
+          />
+        ))}
+      </div>
+    </div>
   );
 };
 
