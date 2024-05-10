bcec1b0db5d8faf11d4d61bd347afbcd3e0e86ff
ingaSiu <ingasiud@gmail.com>
2024-05-10 11:55:25 +0300

    add onClick prop

--- calendar-v1/src/components/Day.jsx
+++ calendar-v1/src/components/Day.jsx
@@ -2,7 +2,7 @@
 const Day = ({ day, onClick }) => {
   const className = `day ${day.value === 'padding' ? 'padding' : ''} ${day.isCurrentDay ? 'currentDay' : ''}`;
   return (
-    <div className={className}>
+    <div onClick={onClick} className={className}>
       {day.value === 'padding' ? '' : day.value}
 
       {day.event && <div className="event">{day.event.title}</div>}
