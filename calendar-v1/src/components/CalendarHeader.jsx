875825c9b5359bcfe3a907956245c0dd271d6f65
ingaSiu <ingasiud@gmail.com>
2024-05-10 11:32:59 +0300

    create header

--- calendar-v1/src/components/CalendarHeader.jsx
+++ calendar-v1/src/components/CalendarHeader.jsx
@@ -1,5 +1,13 @@
 const CalendarHeader = () => {
-  return <div>CalendarHeader</div>;
+  return (
+    <div id="header">
+      <div id="monthDisplay"></div>
+      <div>
+        <button id="backButton">Back</button>
+        <button id="nextButton">Next</button>
+      </div>
+    </div>
+  );
 };
 
 export default CalendarHeader;
