c4a7639a062e16e066af254779e43c9a41e0d09c
ingaSiu <ingasiud@gmail.com>
2024-05-10 12:12:38 +0300

    create delete modal

--- calendar-v1/src/components/DeleteEventModal.jsx
+++ calendar-v1/src/components/DeleteEventModal.jsx
@@ -1,5 +1,23 @@
-const DeleteEventModal = () => {
-  return <div>DeleteEventModal</div>;
+/* eslint-disable react/prop-types */
+const DeleteEventModal = ({ onDelete, eventText, onClose }) => {
+  return (
+    <>
+      <div id="deleteEventModal">
+        <h2>Event</h2>
+
+        <p id="eventText">{eventText}</p>
+
+        <button onClick={onDelete} id="deleteButton">
+          Delete
+        </button>
+        <button onClick={onClose} id="closeButton">
+          Close
+        </button>
+      </div>
+
+      <div id="modalBackDrop"></div>
+    </>
+  );
 };
 
 export default DeleteEventModal;
