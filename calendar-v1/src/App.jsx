77d42a628c3cb56774ccd3e98cba74bc3804d0c9
ingaSiu <ingasiud@gmail.com>
2024-05-10 10:50:10 +0300

    add more states, helper function

--- calendar-v1/src/App.jsx
+++ calendar-v1/src/App.jsx
@@ -2,10 +2,16 @@ import { useState } from 'react';
 
 const App = () => {
   const [nav, setNav] = useState(0);
+  const [days, setDays] = useState([]);
+  const [dateDisplay, setDateDisplay] = useState('');
   const [clicked, setClicked] = useState();
   const [events, setEvents] = useState(
     localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [],
   );
+
+  const eventForDate = (date) => {
+    events.find((e) => e.date === date);
+  };
   return (
     <>
       <div>hello</div>
