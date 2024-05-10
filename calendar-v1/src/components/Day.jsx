e86259bdde6c6ad9d21af57a2dc191af75600043
ingaSiu <ingasiud@gmail.com>
2024-05-10 11:46:25 +0300

    render calendar days

--- calendar-v1/src/components/Day.jsx
+++ calendar-v1/src/components/Day.jsx
@@ -1,5 +1,13 @@
-const Day = () => {
-  return <div>Day</div>;
+/* eslint-disable react/prop-types */
+const Day = ({ day, onClick }) => {
+  const className = `day ${day.value === 'padding' ? 'padding' : ''} ${day.isCurrentDay ? 'currentDay' : ''}`;
+  return (
+    <div className={className}>
+      {day.value === 'padding' ? '' : day.value}
+
+      {day.event && <div className="event">{day.event.title}</div>}
+    </div>
+  );
 };
 
 export default Day;
