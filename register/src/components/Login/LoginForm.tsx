0effb6b4f5d43544f111fbe9e2d127bf7c684ef5
ingaSiu <ingasiud@gmail.com>
2024-05-25 08:38:27 +0300

    add login form

--- register/src/components/Login/LoginForm.tsx
+++ register/src/components/Login/LoginForm.tsx
@@ -1,5 +1,36 @@
+import styles from './LoginForm.module.scss';
+import { useForm } from 'react-hook-form';
+
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className={styles.inputWrapper}>
+        <label htmlFor="email">User Email</label>
+        <input type="email" {...register('email')} />
+        {errors.email && <p className={styles.errorMsg}>{errors.email.message}</p>}
+      </div>
+      <div className={styles.inputWrapper}>
+        <label htmlFor="password">Password</label>
+        <input type="password" {...register('password')} />
+        {errors.password && <p className={styles.errorMsg}>{errors.password.message}</p>}
+      </div>
+
+      <button className={styles.loginBtn} type="submit">
+        Login
+      </button>
+    </form>
+  );
 };
 
 export default LoginForm;
