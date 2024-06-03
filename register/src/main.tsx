0880cb3bd994f826dba10c270c77b9b61d4c89f7
ingaSiu <ingasiud@gmail.com>
2024-06-03 18:48:04 +0300

    wrap app in context

--- register/src/main.tsx
+++ register/src/main.tsx
@@ -4,11 +4,14 @@ import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { UserProvider } from './context/UserContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
