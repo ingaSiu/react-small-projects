02bdba4bebc21adcc0d601226ac267f94911e6de
ingaSiu <ingasiud@gmail.com>
2024-06-07 08:15:14 +0300

    add email and psw validation in be

--- register-v2/backend/models/userModel.js
+++ register-v2/backend/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-
 const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const Schema = mongoose.Schema;
 
@@ -19,6 +19,18 @@ const userSchema = new Schema({
 // static sign up method
 
 userSchema.statics.signup = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error('All fields must be filled');
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error('Email is not valid');
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error('Password not strong enough');
+  }
+
   const exists = await this.findOne({ email });
 
   if (exists) {
@@ -26,7 +38,6 @@ userSchema.statics.signup = async function (email, password) {
   }
 
   const salt = await bcrypt.genSalt(10);
-
   const hash = await bcrypt.hash(password, salt);
 
   const user = await this.create({ email, password: hash });
