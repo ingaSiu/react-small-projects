510852dfbfb20bb8b47d5dd55d9d68af277dedfc
ingaSiu <ingasiud@gmail.com>
2024-06-10 08:29:09 +0300

    finish login functionality

--- register-v2/frontend/src/pages/Login.js
+++ register-v2/frontend/src/pages/Login.js
@@ -1,13 +1,15 @@
+import { useLogin } from '../hooks/useLogin';
 import { useState } from 'react';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { login, error, isLoading } = useLogin();
 
   const handleSubmit = async () => {
     e.preventDefault();
 
-    console.log(email, password);
+    await login(email, password);
   };
 
   return (
@@ -20,7 +22,9 @@ const Login = () => {
       <label>Password:</label>
       <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
 
-      <button>Log in</button>
+      <button disabled={isLoading}>Log in</button>
+
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
