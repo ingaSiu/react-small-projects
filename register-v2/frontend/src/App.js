942e63aee47d810ca963fdbbc8967c644c829564
ingaSiu <ingasiud@gmail.com>
2024-06-12 14:01:38 +0300

    redirect user based on authentication status

--- register-v2/frontend/src/App.js
+++ register-v2/frontend/src/App.js
@@ -1,21 +1,23 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 // pages & components
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Navbar from './components/Navbar';
 import Signup from './pages/Signup';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+  const { user } = useAuthContext();
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+            <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
+            <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
           </Routes>
         </div>
       </BrowserRouter>
