cb7a021347c496508082de6a648340e0bb430e25
ingaSiu <ingasiud@gmail.com>
2024-06-12 16:27:39 +0300

    clear global workout state

--- register-v2/frontend/src/hooks/useLogout.js
+++ register-v2/frontend/src/hooks/useLogout.js
@@ -1,7 +1,9 @@
 import { useAuthContext } from './useAuthContext';
+import { useWorkoutsContext } from './useWorkoutsContext';
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
+  const { dispatch: workoutsDispatch } = useWorkoutsContext();
 
   const logout = () => {
     // remove user fro  storage
@@ -9,6 +11,9 @@ export const useLogout = () => {
 
     // dispatch logout action
     dispatch({ type: 'LOGOUT' });
+
+    // clear global workouts state, so then logged in we dont see all data before user's data loads
+    workoutsDispatch({ type: 'SET_WORKOUTS', payload: null });
   };
 
   return { logout };
