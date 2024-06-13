f2cd3758f9edb4e7f5bef15124108c7139db0a14
ingaSiu <ingasiud@gmail.com>
2024-06-13 15:39:05 +0300

    add all user controller functions

--- register-v3/backend/controllers/userController.js
+++ register-v3/backend/controllers/userController.js
@@ -8,4 +8,36 @@ const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Auth user' });
 });
 
-export { authUser };
+// @desc    Register a new user
+// route    POST /api/users
+// @access  Public
+
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Register user' });
+});
+
+// @desc    Logout user
+// route    POST /api/users/logout
+// @access  Public
+
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Logout user' });
+});
+
+// @desc    Get user profile
+// route    GET /api/users/profile
+// @access  Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'User profile' });
+});
+
+// @desc    Update user profile
+// route    Put /api/users/profile
+// @access  Private
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Update user profile' });
+});
+
+export { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile };
