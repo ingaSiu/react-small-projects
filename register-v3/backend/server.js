bb381d50b3dbb91162e579a797b91c42599b6b14
ingaSiu <ingasiud@gmail.com>
2024-06-13 15:24:31 +0300

    pass error middleware to server

--- register-v3/backend/server.js
+++ register-v3/backend/server.js
@@ -1,3 +1,5 @@
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+
 import dontenv from 'dotenv';
 import express from 'express';
 import userRoutes from './routes/userRoutes.js';
@@ -11,5 +13,8 @@ const app = express();
 app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => res.send('Server is ready'));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
