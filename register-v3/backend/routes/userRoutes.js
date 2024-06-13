6a059ff7e078256ba16ffa19245aa933a138bfd4
ingaSiu <ingasiud@gmail.com>
2024-06-13 18:03:05 +0300

    connect all routes to controller functions

--- register-v3/backend/routes/userRoutes.js
+++ register-v3/backend/routes/userRoutes.js
@@ -1,8 +1,18 @@
-import { authUser } from '../controllers/userController.js';
+import {
+  authUser,
+  getUserProfile,
+  logoutUser,
+  registerUser,
+  updateUserProfile,
+} from '../controllers/userController.js';
+
 import express from 'express';
 
 const router = express.Router();
 
+router.post('/', registerUser);
 router.post('/auth', authUser);
+router.post('/logout', logoutUser);
+router.route('/profile').get(getUserProfile).put(updateUserProfile);
 
 export default router;
