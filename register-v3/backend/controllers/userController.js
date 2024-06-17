787923e62ff00556cac500f57aa7aa231255cecf
ingaSiu <ingasiud@gmail.com>
2024-06-17 18:29:17 +0300

    generate token while register

--- register-v3/backend/controllers/userController.js
+++ register-v3/backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from '../models/userModel.js';
 import asyncHandler from 'express-async-handler';
+import generateToken from '../utils/generateToken.js';
 
 // all the model methods return a promise!
 
@@ -32,6 +33,7 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   if (user) {
+    generateToken(res, user._id);
     res.status(201).json({
       _id: user._id,
       name: user.name,
