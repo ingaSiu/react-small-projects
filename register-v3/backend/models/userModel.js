aebed8d3f0e25841f5e9ab56d6c0bc6ca53c7f67
ingaSiu <ingasiud@gmail.com>
2024-06-17 09:28:29 +0300

    hash psw

--- register-v3/backend/models/userModel.js
+++ register-v3/backend/models/userModel.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcryptjs';
 import mongoose from 'mongoose';
 
 const userSchema = mongoose.Schema(
@@ -20,6 +21,16 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   },
 );
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  // it is like a key we have to useb to hash a psw
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model('User', userSchema);
 
