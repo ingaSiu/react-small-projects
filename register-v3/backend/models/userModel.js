e247bd25bce0f7ce644531d1e1213ea58c74dfb0
ingaSiu <ingasiud@gmail.com>
2024-06-17 18:37:45 +0300

    check psw

--- register-v3/backend/models/userModel.js
+++ register-v3/backend/models/userModel.js
@@ -32,6 +32,11 @@ userSchema.pre('save', async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
+// compare plain text psw with hashed - checks psw, return true or false
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
