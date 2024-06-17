f9f2590814bf3a594bf6abcae3db9b3d263c8981
ingaSiu <ingasiud@gmail.com>
2024-06-17 18:38:07 +0300

    check user email and psw on authUser

--- register-v3/backend/controllers/userController.js
+++ register-v3/backend/controllers/userController.js
@@ -9,7 +9,22 @@ import generateToken from '../utils/generateToken.js';
 // @access  Public
 
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Auth user' });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  // check for email and psw
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
 });
 
 // @desc    Register a new user
