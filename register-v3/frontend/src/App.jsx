b9f363118fd336f4dabdb1c6799b300a49547640
ingaSiu <ingasiud@gmail.com>
2024-06-19 08:01:20 +0300

    add component

--- register-v3/frontend/src/App.jsx
+++ register-v3/frontend/src/App.jsx
@@ -1,10 +1,13 @@
+import { Container } from 'react-bootstrap';
 import Header from './components/Header';
-import HomeScreen from './screens/HomeScreen';
+import { Outlet } from 'react-router-dom';
 const App = () => {
   return (
     <>
       <Header />
-      <HomeScreen />
+      <Container className="my-2">
+        <Outlet />
+      </Container>
     </>
   );
 };
