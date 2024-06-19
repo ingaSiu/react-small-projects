25a28d593e0d36841cb874798ab79c30b1500467
ingaSiu <ingasiud@gmail.com>
2024-06-19 08:57:28 +0300

    create register ui

---
+++ register-v3/frontend/src/screens/RegisterScreen.jsx
@@ -0,0 +1,76 @@
+import { Button, Col, Form, Row } from 'react-bootstrap';
+
+import FormContainer from '../components/FormContainer';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+const RegisterScreen = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log('submit');
+  };
+  return (
+    <FormContainer>
+      <h1>Register</h1>
+
+      <Form onSubmit={submitHandler}>
+        <Form.Group className="my-2" controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="my-2" controlId="email">
+          <Form.Label>Email Adress</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="my-2" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="my-2" controlId="confirmPassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Confirm password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type="submit" variant="primary" className="mt-3">
+          Register
+        </Button>
+
+        <Row className="py-3">
+          <Col>
+            Already have an account? <Link to="/login">Login</Link>
+          </Col>
+        </Row>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default RegisterScreen;
