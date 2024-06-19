b737ba8f1664a29317527a1eed7d23486d63ce70
ingaSiu <ingasiud@gmail.com>
2024-06-19 09:23:35 +0300

    create auth slice for local storage

---
+++ register-v3/frontend/src/slices/authSlice.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+
+import { createSlice } from '@reduxjs/toolkit';
+
+// initial state will check local storage
+// JSON.parse will turn info to js object from the string which is saved in localStorage
+const initialState = {
+  userInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setCredentials: (state, action) => {
+      state.userInfo = action.payload;
+      localStorage.setItem('userInfo', JSON.stringify(action.payload));
+    },
+    logout: (state, action) => {
+      state.userInfo = null;
+      localStorage.removeItem('userInfo');
+    },
+  },
+});
+
+export const { setCredentials, logout } = authSlice.actions;
+
+export default authSlice.reducer;
