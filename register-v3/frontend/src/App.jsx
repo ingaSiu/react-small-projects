9df749127c36a78bd755e39e8e48899d65cfc420
ingaSiu <ingasiud@gmail.com>
2024-06-20 11:40:07 +0300

    add toast to app

--- register-v3/frontend/src/App.jsx
+++ register-v3/frontend/src/App.jsx
@@ -1,10 +1,15 @@
+import 'react-toastify/dist/ReactToastify.css';
+
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
 const App = () => {
   return (
     <>
       <Header />
+      <ToastContainer />
       <Container className="my-2">
         <Outlet />
       </Container>
