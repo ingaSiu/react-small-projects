662711ccbaec1e01f2392130696ad5295558933c
ingaSiu <ingasiud@gmail.com>
2024-06-20 14:30:42 +0300

    add private route

--- register-v3/frontend/src/main.jsx
+++ register-v3/frontend/src/main.jsx
@@ -6,6 +6,8 @@ import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } 
 import App from './App.jsx';
 import HomeScreen from './screens/HomeScreen.jsx';
 import LoginScreen from './screens/LoginScreen.jsx';
+import PrivateRoute from './components/PrivateRoute.jsx';
+import ProfileScreen from './screens/ProfileScreen.jsx';
 import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -18,6 +20,10 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
+      {/* Private routes */}
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/profile" element={<ProfileScreen />} />
+      </Route>
     </Route>,
   ),
 );
