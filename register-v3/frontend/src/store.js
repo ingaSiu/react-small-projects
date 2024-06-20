aa4cc3621bc33a6664c0940219c23d3cd509af46
ingaSiu <ingasiud@gmail.com>
2024-06-20 10:52:10 +0300

    add apiSlice with middleware

--- register-v3/frontend/src/store.js
+++ register-v3/frontend/src/store.js
@@ -1,11 +1,13 @@
+import { apiSlice } from './slices/apiSlice';
 import authReducer from './slices/authSlice';
 import { configureStore } from '@reduxjs/toolkit';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
 });
 
