6fd5e6750aaaf95818a555835f3c5e2e25e58609
ingaSiu <ingasiud@gmail.com>
2024-07-08 06:59:24 +0300

    add routes to app

--- register-v4/client/src/App.jsx
+++ register-v4/client/src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import About from './pages/About';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
 const App = () => {
-  return <div className="text-red-900">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
