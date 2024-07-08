955d70f2b4c7ae768b8f5bdff765bc4d641170ac
ingaSiu <ingasiud@gmail.com>
2024-07-08 07:43:36 +0300

    create header component

--- register-v4/client/src/App.jsx
+++ register-v4/client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import About from './pages/About';
+import Header from './components/Header';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
@@ -9,6 +10,7 @@ import SignUp from './pages/SignUp';
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
