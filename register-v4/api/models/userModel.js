f7281251ac817f1e51165a075e7b82991227e0ba
ingaSiu <ingasiud@gmail.com>
2024-07-09 14:50:00 +0300

    create user model

---
+++ register-v4/api/models/userModel.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+// a schema are the rules and conditions we want to add to the user
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
