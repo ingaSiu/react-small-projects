cebecd32763dc01ad3703be7ece204906d25ffd2
ingaSiu <ingasiud@gmail.com>
2024-07-11 16:15:45 +0300

    add error

--- register-v4/api/controllers/authController.js
+++ register-v4/api/controllers/authController.js
@@ -4,8 +4,10 @@ export const signup = async (req, res) => {
   const { username, email, password } = req.body;
 
   const newUser = new User({ username, email, password });
-
-  await newUser.save();
-
-  res.status(201).json({ message: 'User created succesfully' });
+  try {
+    await newUser.save();
+    res.status(201).json({ message: 'User created succesfully' });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 };
