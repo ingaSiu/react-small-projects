53b17f2dc2831dd7bf5b1ce398233cf077efa1da
ingaSiu <ingasiud@gmail.com>
2024-07-11 16:13:25 +0300

    send user data

--- register-v4/api/controllers/authController.js
+++ register-v4/api/controllers/authController.js
@@ -1,3 +1,11 @@
-export const signup = (req, res) => {
-  console.log(req.body);
+import User from '../models/userModel.js';
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const newUser = new User({ username, email, password });
+
+  await newUser.save();
+
+  res.status(201).json({ message: 'User created succesfully' });
 };
