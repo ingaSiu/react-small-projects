07fbce250a9e84bec705e21af62ac2aab5f49fad
ingaSiu <ingasiud@gmail.com>
2024-07-11 16:05:48 +0300

    add a fix

--- register-v4/api/index.js
+++ register-v4/api/index.js
@@ -16,11 +16,18 @@ mongoose
   });
 
 const app = express();
-
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
-});
+app.use(express.json());
 
 app.use('/api/user', userRoutes);
 
 app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'API is working!',
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
