3202d824c9691bff73b83c1bc8dd3f8852a53449
ingaSiu <ingasiud@gmail.com>
2024-07-12 11:53:00 +0300

    add loading and error states

--- register-v4/client/src/pages/SignUp.jsx
+++ register-v4/client/src/pages/SignUp.jsx
@@ -3,6 +3,8 @@ import { useState } from 'react';
 
 const SignUp = () => {
   const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -11,14 +13,23 @@ const SignUp = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch('/api/auth/signup', {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    });
-    const data = await res.json();
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      setLoading(false);
+      setError(false);
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
   };
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -47,8 +58,11 @@ const SignUp = () => {
           onChange={handleChange}
         />
 
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-70">
-          Sign up
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-70"
+        >
+          {loading ? 'Loading' : 'Sign up'}
         </button>
       </form>
 
@@ -58,6 +72,7 @@ const SignUp = () => {
           <span className="text-blue-500">Sign in</span>
         </Link>
       </div>
+      {error && <p className="text-red-700 font-semibold mt-5">Something went wrong</p>}
     </div>
   );
 };
