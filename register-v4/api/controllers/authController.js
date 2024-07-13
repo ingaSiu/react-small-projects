cd861da08b07c06cfd7969306c96a3513a611858
ingaSiu <ingasiud@gmail.com>
2024-07-13 17:44:13 +0300

    add cookie

--- register-v4/api/controllers/authController.js
+++ register-v4/api/controllers/authController.js
@@ -1,6 +1,7 @@
 import User from '../models/userModel.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -20,10 +21,21 @@ export const signin = async (req, res, next) => {
 
   try {
     const validUser = await User.findOne({ email });
-
     if (!validUser) return next(errorHandler(404, 'User not found'));
+
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, 'Wrong credentials'));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    // remove psw from the client side info,
+    // _doc is for destructurizing properly
+
+    const { password: hashedPassword, ...rest } = validUser._doc;
+
+    const expiryDate = new Date(Date.now() + 3600000); // expires in 1 hour
+
+    res.cookie('access_token', token, { httpOnly: true, expires: expiryDate }).status(200).json(rest);
   } catch (error) {
     next(error);
   }
