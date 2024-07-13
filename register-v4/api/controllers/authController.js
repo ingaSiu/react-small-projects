140247024f1cc7a19ccd7a7c2a5c391d43ff8806
ingaSiu <ingasiud@gmail.com>
2024-07-13 17:17:14 +0300

    check for correct email and psw

--- register-v4/api/controllers/authController.js
+++ register-v4/api/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from '../models/userModel.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -9,6 +10,20 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: 'User created succesfully' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) return next(errorHandler(404, 'User not found'));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials'));
   } catch (error) {
     next(error);
   }
