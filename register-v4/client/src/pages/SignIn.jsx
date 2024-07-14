b12c40b83c7f7503a33f30b868b0e04f241845e1
ingaSiu <ingasiud@gmail.com>
2024-07-14 20:38:01 +0300

    add sign in functionality

--- register-v4/client/src/pages/SignIn.jsx
+++ register-v4/client/src/pages/SignIn.jsx
@@ -1,5 +1,77 @@
+import { Link, useNavigate } from 'react-router-dom';
+
+import { useState } from 'react';
+
 const SignIn = () => {
-  return <div>Signin</div>;
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      setLoading(false);
+      setError(false);
+      navigate('/');
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign in</h1>
+
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-70"
+        >
+          {loading ? 'Loading' : 'Sign in'}
+        </button>
+      </form>
+
+      <div className="flex gap-2 mt-5">
+        <p>Don&#39;t have an account?</p>
+        <Link to="/sign-up">
+          <span className="text-blue-500">Sign up</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-700 font-semibold mt-5">Something went wrong</p>}
+    </div>
+  );
 };
 
 export default SignIn;
