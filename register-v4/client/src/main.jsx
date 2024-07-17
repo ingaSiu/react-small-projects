d8bebf63a15eab62b0ab419efe17c80c72f1d828
ingaSiu <ingasiud@gmail.com>
2024-07-17 09:13:10 +0300

    wrap app in persist

--- register-v4/client/src/main.jsx
+++ register-v4/client/src/main.jsx
@@ -1,15 +1,19 @@
 import './index.css';
 
+import { persistor, store } from './redux/store.js';
+
 import App from './App.jsx';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { store } from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <PersistGate persistor={persistor} loading={null}>
+        <App />
+      </PersistGate>
     </React.StrictMode>
   </Provider>,
 );
