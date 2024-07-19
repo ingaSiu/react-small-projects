2f4f83580556450ab7ff610bc09602e2433a8eb7
ingaSiu <ingasiud@gmail.com>
2024-07-19 15:34:06 +0300

    add user's profile pic to header

--- register-v4/client/src/components/Header.jsx
+++ register-v4/client/src/components/Header.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="bg-slate-200">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3 ">
@@ -18,7 +20,11 @@ const Header = () => {
           </Link>
 
           <Link to="/sign-in">
-            <li>Sign In</li>
+            {currentUser ? (
+              <img src={currentUser.profilePicture} alt="user" className="h-8 w-8 rounded-full object-cover" />
+            ) : (
+              <li>Sign In</li>
+            )}
           </Link>
         </ul>
       </div>
