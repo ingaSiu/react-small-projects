126c33d5a9883fde0f4b36d5decfbd70c459aa37
ingaSiu <ingasiud@gmail.com>
2024-07-19 15:45:35 +0300

    wrap profile in private page

--- register-v4/client/src/App.jsx
+++ register-v4/client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import About from './pages/About';
 import Header from './components/Header';
 import Home from './pages/Home';
+import PrivateRoute from './components/PrivateRoute';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
@@ -16,7 +17,9 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
