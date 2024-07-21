fcba879c13c2eec6c1f125985dcafe55e15dab38
ingaSiu <ingasiud@gmail.com>
2024-07-21 17:16:37 +0300

    update error with custom function

--- register-v4/api/utils/verifyUser.js
+++ register-v4/api/utils/verifyUser.js
@@ -1,12 +1,13 @@
+import { errorHandler } from './error.js';
 import jwt from 'jsonwebtoken';
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
 
-  if (!token) return res.status(401).json('You need to log in');
+  if (!token) return next(errorHandler, (401, 'You need to log in'));
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.status(403).json('Token is not valid');
+    if (err) return next(errorHandler(403, 'Token is not valid'));
 
     req.user = user;
     next();
