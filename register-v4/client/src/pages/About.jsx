886bc464343254dedd662c156af1ca34b08dc375
ingaSiu <ingasiud@gmail.com>
2024-07-24 16:29:41 +0300

    add content to pages

--- register-v4/client/src/pages/About.jsx
+++ register-v4/client/src/pages/About.jsx
@@ -1,5 +1,14 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-700">About</h1>
+      <p className="mb-4 text-slate-800">
+        This is a full-stack web application built with the MERN (MongoDB, Express, React, Node.js) stack. It includes
+        authentication features that allow users to sign up, log in, and log out, and provides access to protected
+        routes only for authenticated users.
+      </p>
+    </div>
+  );
 };
 
 export default About;
